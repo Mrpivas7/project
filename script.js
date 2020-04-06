a868a7e94bcc0918a6f9e2992c23c769d7e840c0
Andriy Paliy <mr.pivas7@gmail.com>
2020-04-06 17:06:59 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+'use strict';
+
+let arr = ['plum.png','orange.jpg','apple.bmp'];
+
+ console.log(arr[0]);
+
+// console.log(typeof(null));
+// alert("Hello World!")
+//let answer = confirm("Are you here?");
+
+// console.log(answer);
+
+//let answer = +prompt("Вам є 18?", "Так");
+
+// console.log(typeof(answer));
+
+// console.log ("arr" + " - object");
+// console.log(4 + +" - object");
+
+let incr = 10,
+    decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isChecked = false,
+    isClose = false;
+console.log(isChecked || !isClose);
+
+
